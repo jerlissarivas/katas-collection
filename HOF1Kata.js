b36d8fb947bbc3a9e5637800c4113041fd6e6085
Jerlissa Rivas <jerlissa.rivas@gmail.com>
2020-03-14 18:10:20 -0400

    Add another solution and better solution

--- HOF1Kata.js
+++ HOF1Kata.js
@@ -3,15 +3,65 @@
 // Your task is to return the number of JavaScript developers coming from Europe.
 
 var list1 = [
-    { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'JavaScript' },
-    { firstName: 'Maia', lastName: 'S.', country: 'Tahiti', continent: 'Europe', age: 28, language: 'JavaScript' },
-    { firstName: 'Shufen', lastName: 'L.', country: 'Taiwan', continent: 'Asia', age: 35, language: 'HTML' },
-    { firstName: 'Sumayah', lastName: 'M.', country: 'Tajikistan', continent: 'Asia', age: 30, language: 'CSS' }
-  ];
-  
-  
-  const europeDevs = list1.filter(devs => (
-   devs.continent.includes('Europe') && devs.language.includes('JavaScript')
-  ));
-  
-  console.log(europeDevs.length)
+  {
+    firstName: "Noah",
+    lastName: "M.",
+    country: "Switzerland",
+    continent: "Europe",
+    age: 19,
+    language: "JavaScript"
+  },
+  {
+    firstName: "Maia",
+    lastName: "S.",
+    country: "Tahiti",
+    continent: "Europe",
+    age: 28,
+    language: "JavaScript"
+  },
+  {
+    firstName: "Shufen",
+    lastName: "L.",
+    country: "Taiwan",
+    continent: "Asia",
+    age: 35,
+    language: "HTML"
+  },
+  {
+    firstName: "Sumayah",
+    lastName: "M.",
+    country: "Tajikistan",
+    continent: "Asia",
+    age: 30,
+    language: "CSS"
+  }
+];
+
+// My solution
+
+const europeDevs = list1.filter(
+  devs =>
+    devs.continent.includes("Europe") && devs.language.includes("JavaScript")
+);
+
+console.log(europeDevs.length);
+
+// Another Solution
+
+const countDevelopers = list => {
+  let counter = 0;
+  for (let i of list) {
+    if (i.continent === "Europe" && i.language === "JavaScript") {
+      counter++;
+    }
+  }
+  return counter;
+};
+
+// Better Solution
+
+const countDevelopers = list =>
+  list.filter(x => x.continent == "Europe" && x.language == "JavaScript")
+    .length;
+
+countDevelopers(list1);
