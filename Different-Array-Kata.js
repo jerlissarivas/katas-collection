4fbf40b204edc4210ae2cd1f8b88a1d0bb533cb2
Jerlissa Rivas <jerlissa.rivas@gmail.com>
2020-06-19 23:37:54 -0400

    Complete Different Array Kata

---
+++ Different-Array-Kata.js
@@ -0,0 +1,21 @@
+// Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+
+// It should remove all values from list a, which are present in list b.
+
+// array_diff([1,2],[1]) == [2]
+// If a value is present in b, all of its occurrences must be removed from the other:
+
+// array_diff([1,2,2,2,3],[2]) == [1,3]
+
+function array_diff(a, b) {
+  let diffArray = [];
+
+  for (let i = 0; i < a.length; i++) {
+    let elem = a[i];
+    if (b.indexOf(elem) === -1) {
+      diffArray.push(elem);
+    }
+  }
+  return diffArray;
+}
+array_diff([1, 2], [1]); // Will result in [2]
